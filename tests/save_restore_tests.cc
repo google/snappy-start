c1a0c484a78fa73c2c3299143226c9b73cfc1741
Mark Seaborn <mseaborn@chromium.org>
2015-06-19 17:03:56 +0000

    Correction to mmap() error checks

--- tests/save_restore_tests.cc
+++ tests/save_restore_tests.cc
@@ -10,14 +10,14 @@ int main() {
   int size = getpagesize();
   void *addr = mmap(NULL, size, PROT_READ | PROT_WRITE,
                     MAP_PRIVATE | MAP_ANON, -1, 0);
-  assert(addr != NULL);
+  assert(addr != MAP_FAILED);
   int rc = munmap(addr, size);
   assert(rc == 0);
 
   // Test munmap() that splits up an existing mapping.
   void *addr2 = mmap(NULL, size * 3, PROT_READ | PROT_WRITE,
                      MAP_PRIVATE | MAP_ANON, -1, 0);
-  assert(addr2 != NULL);
+  assert(addr2 != MAP_FAILED);
   ((char *) addr2)[0] = 'a';
   ((char *) addr2)[size * 2] = 'b';
   rc = munmap((char *) addr2 + size, size);
