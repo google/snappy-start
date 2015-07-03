9ac9f270b47be813f398ac225309a694650a3e9f
Mark Seaborn <mseaborn@chromium.org>
2015-07-02 17:27:43 -0700

    save_restore_tests.cc: Start splitting into separate test cases
    These can be run in separate processes.
    
    When a test case fails, this will make it easier to tell why.

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -20,7 +20,15 @@ def Main():
 
   RunTest(['./out/example_prog'])
   RunTest(['./out/example_prog2'])
-  RunTest(['./out/save_restore_tests'])
+
+  # Get list of sub-test names.
+  proc = subprocess.Popen(['./out/save_restore_tests'], stdout=subprocess.PIPE)
+  stdout = proc.communicate()[0]
+  test_names = stdout.strip().split('\n')
+
+  for test_name in test_names:
+    RunTest(['./out/save_restore_tests', test_name])
+
   RunTest(['/usr/bin/python', 'tests/python_example.py'])
 
 
--- tests/save_restore_tests.cc
+++ tests/save_restore_tests.cc
@@ -7,14 +7,22 @@
 #include <errno.h>
 #include <signal.h>
 #include <stdint.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/mman.h>
 #include <sys/syscall.h>
 #include <sys/time.h>
 #include <unistd.h>
 
 
-int main(int argc, char **argv, char **envp) {
+namespace {
+
+void do_snapshot() {
+  raise(SIGUSR1);
+}
+
+void test_munmap() {
   // Test munmap() of an entire mapping.
   int size = getpagesize();
   void *addr = mmap(NULL, size, PROT_READ | PROT_WRITE,
@@ -23,13 +31,18 @@ int main(int argc, char **argv, char **envp) {
   int rc = munmap(addr, size);
   assert(rc == 0);
 
+  do_snapshot();
+}
+
+void test_rest() {
   // Test munmap() that splits up an existing mapping.
+  int size = getpagesize();
   void *addr2 = mmap(NULL, size * 3, PROT_READ | PROT_WRITE,
                      MAP_PRIVATE | MAP_ANON, -1, 0);
   assert(addr2 != MAP_FAILED);
   ((char *) addr2)[0] = 'a';
   ((char *) addr2)[size * 2] = 'b';
-  rc = munmap((char *) addr2 + size, size);
+  int rc = munmap((char *) addr2 + size, size);
   assert(rc == 0);
 
   // Test mprotect() of an entire mapping.
@@ -62,7 +75,7 @@ int main(int argc, char **argv, char **envp) {
   }
 
   // Test that the auxv's pointers to the VDSO have been removed.
-  char **envp_end = envp;
+  char **envp_end = environ;
   while (*envp_end)
     envp_end++;
   Elf64_auxv_t *auxv = (Elf64_auxv_t *) (envp_end + 1);
@@ -71,7 +84,7 @@ int main(int argc, char **argv, char **envp) {
     assert(auxv->a_type != AT_SYSINFO_EHDR);
   }
 
-  raise(SIGUSR1);
+  do_snapshot();
 
   assert(((char *) addr2)[0] == 'a');
   assert(((char *) addr2)[size * 2] == 'b');
@@ -91,6 +104,46 @@ int main(int argc, char **argv, char **envp) {
   struct timeval time;
   rc = gettimeofday(&time, NULL);
   assert(rc == 0);
+}
 
-  return 0;
+struct TestCase {
+  const char *test_name;
+  void (*test_func)();
+};
+
+const TestCase test_cases[] = {
+#define TEST_CASE(NAME) { #NAME, NAME }
+  TEST_CASE(test_munmap),
+  TEST_CASE(test_rest),
+#undef TEST_CASE
+};
+
+}
+
+int main(int argc, char **argv) {
+  size_t test_count = sizeof(test_cases) / sizeof(test_cases[0]);
+
+  if (argc == 1) {
+    // Print list of test cases.
+    for (size_t i = 0; i < test_count; ++i) {
+      printf("%s\n", test_cases[i].test_name);
+    }
+    return 0;
+  }
+
+  if (argc == 2) {
+    // Run one test case.
+    const char *test_name = argv[1];
+    for (size_t i = 0; i < test_count; ++i) {
+      if (strcmp(test_cases[i].test_name, test_name) == 0) {
+        test_cases[i].test_func();
+        return 0;
+      }
+    }
+    printf("Test case not found: %s\n", test_name);
+    return 1;
+  }
+
+  printf("Usage: %s [test_name]\n", argv[0]);
+  return 1;
 }
